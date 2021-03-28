e3ce7bdcd444ddd37d9926a8511c77c309aeff0b
Juan <40309961+JuanGonzalez99@users.noreply.github.com>
2021-03-28 10:53:26 -0300

    slots & named slots

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!-- <Persona nombre="Jorge" edad="25"></Persona>
-  <hr>
-  <Persona nombre="Linda" edad="32"></Persona> -->
-  <Usuarios 
-    v-for="user in users"
-    v-bind:key="user.id"
-    :id="user.id"
-    :name="user.name"
-    :email="user.email"
-    :phone="user.phone">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <Persona nombre="Jorge" edad="25"></Persona>
+    <hr>
+    <Persona nombre="Linda" edad="32"></Persona> -->
+    <!-- <Usuarios 
+        v-for="user in users"
+        v-bind:key="user.id"
+        :id="user.id"
+        :name="user.name"
+        :email="user.email"
+        :phone="user.phone">
 
-  </Usuarios>
+    </Usuarios> -->
+
+    <CompSlot>
+        Test
+        <template v-slot:slotNombrado>
+            Nombrado
+        </template>
+    </CompSlot>
 </template>
 
 <script>
 import axios from 'axios'
+
 // import Persona from './components/Persona'
-import Usuarios from "./components/Usuarios";
+// import Usuarios from "./components/Usuarios";
+import CompSlot from './components/CompSlot';
 
 export default {
-  name: 'App',
-  components: {
-    // Persona,
-    Usuarios
-  },
-  data(){
-    return {
-      users: []
+    name: 'App',
+    components: {
+        // Persona,
+        // Usuarios,
+        CompSlot
+    },
+    data(){
+        return {
+            users: []
+        }
+    },
+    methods:{
+        getUsers(){
+            axios.get('https://jsonplaceholder.typicode.com/users')
+            .then((response) => {
+                this.users = response.data;
+            });
+        }
+    },
+    mounted(){
+        this.getUsers();
     }
-  },
-  methods:{
-    getUsers(){
-      axios.get('https://jsonplaceholder.typicode.com/users')
-        .then((response) => {
-          this.users = response.data;
-        });
-    }
-  },
-  mounted(){
-    this.getUsers();
-  }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/CompSlot.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1 style="color: red">
+            Componente
+            <hr>
+            <slot>
+                Esto se sobreescribe
+            </slot>
+            <br>
+            <slot>
+                Esto también
+            </slot>
+            <br>
+            <slot name="slotNombrado">
+                Este slot se sobreescribe con nombre
+            </slot>
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CompSlot'
+}
+</script>
